9cc683b5a237c247b40d9e70e0e112deedf7d73c
fedyapavlyshynets <fedyapavlyshynets@gmail.com>
2023-11-26 14:54:09 +0200

    update store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,15 @@
-import { configureStore} from "@reduxjs/toolkit";
-import {reducer as movieReducer} from "./slices/slice";
-import {watchlistSlice} from "./slices/watchList";
+import { configureStore } from "@reduxjs/toolkit";
+import { reducer as movieReducer } from "./slices/slice";
+import { watchlistSlice } from "./slices/watchList";
+import { sliderListSlice } from "./slices/sliderDetailsSlice";
+
+
+
 
 export const store = configureStore({
     reducer: {
         movie: movieReducer,
-        watchList: watchlistSlice.reducer
+        watchList: watchlistSlice.reducer,
+        sliderDetails:sliderListSlice.reducer
     }
 })
